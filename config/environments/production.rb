27010e348a927158c524b5959e3e0ebb1a0d8526
RDavies8 <ryscot@gmail.com>
2014-09-25 22:24:52 -0700

    Production Rake

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #----------------------------------#
+    config.assets.compile = true
+    config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  #----------------------------------#
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
---
+++ public/assets/enlarge-2b4d511088e82018d5e8d4e3551ecd58.js
@@ -0,0 +1,1 @@
+function enlarge(){var i=($("#article-image"),$("<div></div>").attr("id","enlarged-display")),a=$(".page-wrapper"),t=$("<img></img>").attr.$display.after(t);a.after(i)}!function(){var i=$(".img-blog");i.bind("click",function(){alert("TEST")})}(this,this.document);
